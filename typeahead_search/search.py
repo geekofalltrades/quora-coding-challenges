188baf8ce5bcf8696d89ee34e283e5e2dcafbcc8
Matt Dougherty <thegeekofalltrades@gmail.com>
2015-02-27 14:31:10 -0800

    [typeahead_search] Implement get_num_commands.

--- typeahead_search/search.py
+++ typeahead_search/search.py
@@ -15,10 +15,23 @@ class TypeAheadSearch(object):
 
         # Loop through individual commands.
         for i in range(self.num_commands):
-            pass
+            command = sys.stdin.readline().strip()
+            if not command:
+                raise ValueError
 
     def get_num_commands(self):
         """Read the number of expected commands from stdin."""
+        try:
+            num_commands = int(sys.stdin.readline().strip())
+        except ValueError:
+            raise TypeError(
+                "First line of input must be an integer."
+            )
+
+        if num_commands <= 0 or num_commands > 20:
+            raise ValueError(
+                "First line of input must be between 1 and 20, inclusive."
+            )
 
     def parse_command(self, command):
         """Parse and validate individual commands."""
