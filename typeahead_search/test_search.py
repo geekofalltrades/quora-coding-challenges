07f025fdca401fa972936df3763ac150bb7535c6
Matt Dougherty <thegeekofalltrades@gmail.com>
2015-02-27 16:42:01 -0800

    [typeahead_search] Stub out test methods for num_commands.

--- typeahead_search/test_search.py
+++ typeahead_search/test_search.py
@@ -3,7 +3,18 @@ import unittest
 
 class TestTypeAheadSearch(unittest.TestCase):
     """Test the TypeAheadSearch class."""
-    pass
+
+    def test_num_commands_int_or_int_string(self):
+        """num_commands can be assigned integers or integer strings."""
+
+    def test_num_commands_not_int(self):
+        """TypeError is raised when num_commands not of type int."""
+
+    def test_num_commands_below_range(self):
+        """ValueError is raised when num_commands < 0."""
+
+    def test_num_commands_above_range(self):
+        """ValueError is raised when num_commands > 20."""
 
 
 if __name__ == '__main__':
