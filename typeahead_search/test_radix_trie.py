b64ff327bdec3dcc7ce01c8ee982909cd78acc77
Matt Dougherty <thegeekofalltrades@gmail.com>
2015-03-07 17:02:49 -0800

    [typeahead_radix_trie] Add red-green tests for Radix Trie delete.

--- typeahead_search/test_radix_trie.py
+++ typeahead_search/test_radix_trie.py
@@ -112,6 +112,100 @@ class TestRadixTrie(unittest.TestCase):
         self.assertIn(self.ids[0], node.entries)
         self.assertIn(self.ids[1], node.entries)
 
+    def test_delete_only_word(self):
+        """Delete the only entry/word from the Trie."""
+        self.trie.add('some', self.ids[0])
+        self.trie.delete('some', self.ids[0])
+        self.assertEqual(len(self.trie.children), 0)
+        self.assertEqual(len(self.trie.entries), 0)
+
+    def test_delete_words_remaining(self):
+        """Delete an entry from a Trie with multiple words."""
+        self.trie.add('some', self.ids[0])
+        self.trie.add('day', self.ids[1])
+        self.trie.delete('some', self.ids[0])
+
+        node = self.trie
+        self.assertEqual(len(node.children), 1)
+        self.assertIn('day', node.children)
+        self.assertEqual(len(node.entries), 1)
+        self.assertIn(self.ids[1], node.entries)
+
+        node = node.children['day']
+        self.assertEqual(len(node.children), 0)
+        self.assertEqual(len(node.entries), 1)
+        self.assertIn(self.ids[1], node.entries)
+
+    def test_delete_word_with_multiple_entries(self):
+        """Delete an entry from a word that has multiple entries."""
+        self.trie.add('some', self.ids[0])
+        self.trie.add('some', self.ids[1])
+        self.trie.delete('some', self.ids[0])
+
+        node = self.trie
+        self.assertEqual(len(node.children), 1)
+        self.assertIn('some', node.children)
+        self.assertEqual(len(node.entries), 1)
+        self.assertIn(self.ids[1], node.entries)
+
+        node = node.children['some']
+        self.assertEqual(len(node.children), 0)
+        self.assertEqual(len(node.entries), 1)
+        self.assertIn(self.ids[1], node.entries)
+
+    def test_delete_only_entry_from_postfix(self):
+        """Delete the only entry from a word that postfixes another word."""
+        self.trie.add('some', self.ids[0])
+        self.trie.add('someday', self.ids[1])
+        self.trie.delete('someday', self.ids[1])
+
+        node = self.trie
+        self.assertEqual(len(node.children), 1)
+        self.assertIn('some', node.children)
+        self.assertEqual(len(node.entries), 1)
+        self.assertIn(self.ids[0], node.entries)
+
+        node = node.children['some']
+        self.assertEqual(len(node.children), 0)
+        self.assertEqual(len(node.entries), 1)
+        self.assertIn(self.ids[0], node.entries)
+
+    def test_delete_postfix_collapse_branch(self):
+        """Delete the only entry from a word that is one of two postfixing
+        another word.
+        """
+        self.trie.add('somebody', self.ids[0])
+        self.trie.add('someday', self.ids[1])
+        self.trie.delete('someday', self.ids[1])
+
+        node = self.trie
+        self.assertEqual(len(node.children), 1)
+        self.assertIn('somebody', node.children)
+        self.assertEqual(len(node.entries), 1)
+        self.assertIn(self.ids[0], node.entries)
+
+        node = node.children['somebody']
+        self.assertEqual(len(node.children), 0)
+        self.assertEqual(len(node.entries), 1)
+        self.assertIn(self.ids[0], node.entries)
+
+    def test_delete_only_entry_from_prefix(self):
+        """Delete the only entry from a word that prefixes another word."""
+        self.trie.add('some', self.ids[0])
+        self.trie.add('someday', self.ids[1])
+        self.trie.delete('some', self.ids[0])
+
+        node = self.trie
+        self.assertEqual(len(node.children), 1)
+        self.assertIn('someday', node.children)
+        self.assertEqual(len(node.entries), 1)
+        self.assertIn(self.ids[0], node.entries)
+
+        node = node.children['someday']
+        self.assertEqual(len(node.children), 0)
+        self.assertEqual(len(node.entries), 1)
+        self.assertIn(self.ids[0], node.entries)
+
     def test_single_search(self):
         """Search for an entry at some word."""
         self.trie.add('some', self.ids[0])
