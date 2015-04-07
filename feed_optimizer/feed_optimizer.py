b9b09aef5b8cfd8f1cf29a3c17ad505e6b0da3d0
Matt Dougherty <thegeekofalltrades@gmail.com>
2015-04-07 16:43:44 -0700

    [feed_optimizer] Break refresh logic into subroutines.

--- feed_optimizer/feed_optimizer.py
+++ feed_optimizer/feed_optimizer.py
@@ -25,6 +25,79 @@ class FeedOptimizerSession(object):
         # browser height for that rule.
         self.rules = []
 
+    def _remove_story(self, story_id):
+        """Remove the story with the given id."""
+        try:
+            story = self.stories_by_id[story_id]
+
+            del self.stories_by_id[story_id]
+
+            self.stories_by_bucket[story.height].remove(story)
+            if not self.stories_by_bucket[story.height]:
+                del self.stories_by_bucket[story.height]
+
+        except (KeyError, ValueError):
+            raise LookupError(
+                "Story with id {} does not exist.".format(story_id)
+            )
+
+    def _prune_stories(self, time):
+        """Remove all stories older than the given time."""
+        try:
+            while self.stories_by_id[self.oldest_story_id].time < time:
+                self._remove_story(self.oldest_story_id)
+                self.oldest_story_id += 1
+
+        # A KeyError is raised in the case where we have no unexpired stories
+        # (self.oldest_story_id is greater than self.current_story_id).
+        except KeyError:
+            pass
+
+    def _build_rules(self):
+        """Build a set of dynamic programming rules for the optimal feed
+        at each browser height up to and including self.browser_height.
+        """
+        rules = [set()]
+
+        for browser_height in range(1, self.browser_height + 1):
+            # Begin with the rule representing the best available feed for
+            # the previous browser height.
+            possible_rules = [rules[browser_height - 1]]
+
+            # See if we can improve on the rule for the previous browser
+            # height by creating a set of candidate rules obtained by
+            # adding the highest-scoring unused story of each available
+            # story height.
+            for story_height, story_bucket in \
+                    self.stories_by_bucket.iteritems():
+                # Skip this bucket if its story height is greater than
+                # the browser height.
+                if browser_height - story_height < 0:
+                    continue
+
+                # Fetch the rule representing the best feed that could
+                # additionally accommodate a story of this height.
+                previous_rule = rules[browser_height - story_height]
+
+                # Find the highest-scoring story of this height not
+                # already used in the previous_rule, if any, and add it
+                # to a new candidate rule.
+                for story in story_bucket:
+                    if story not in previous_rule:
+                        possible_rules.append(previous_rule.copy())
+                        possible_rules[-1].add(story)
+                        break
+
+            # Select the highest-scoring rule from among our candidate rules.
+            rules[browser_height] = max(
+                possible_rules,
+                key=lambda rule: sum(
+                    self.stories_by_id[story_id].score for story_id in rule
+                )
+            )
+
+        return rules
+
     def add_story(self, story_time, story_score, story_height):
         """Add a story."""
         self.current_story_id += 1
@@ -47,91 +120,27 @@ class FeedOptimizerSession(object):
         else:
             bucket.append(new_story)
 
-    def _remove_story(self, story_id):
-        """Remove the story with the given id."""
-        try:
-            story = self.stories_by_id[story_id]
-
-            del self.stories_by_id[story_id]
-
-            self.stories_by_bucket[story.height].remove(story)
-            if not self.stories_by_bucket[story.height]:
-                del self.stories_by_bucket[story.height]
-
-        except (KeyError, ValueError):
-            raise LookupError(
-                "Story with id {} does not exist.".format(story_id)
-            )
-
     def refresh(self, refresh_time):
         """Refresh the page.
         Prune old stories, then build a set of dynamic programming rules
         to determine the optimal feed.
         """
         # Remove old stories.
-        try:
-            while (
-                refresh_time - self.time_window >
-                self.stories_by_id[self.oldest_story_id].time
-            ):
-                self._remove_story(self.oldest_story_id)
-                self.oldest_story_id += 1
+        self._prune_stories(refresh_time - self.time_window)
 
-        # A KeyError is raised in the case where we have no unexpired stories
-        # (self.oldest_story_id is greater than self.current_story_id).
-        # We can short-circuit here and return an empty feed.
-        except KeyError:
+        # If we have no stories remaining, short-circuit and return an
+        # empty feed.
+        if not self.stories_by_id:
             return "0 0"
 
-        # Build dynamic programming rules.
-        rules = [set()]
+        # Get the set of stories representing the optimal feed.
+        rule = self._build_rules()[self.browser_height]
 
-        for browser_height in range(1, self.browser_height + 1):
-            # Begin with the rule representing the best available feed for
-            # the previous browser height.
-            possible_rules = [rules[browser_height - 1]]
-
-            # See if we can improve on the rule for the previous browser
-            # height by creating a set of candidate rules obtained by
-            # adding the highest-scoring unused story of each available
-            # story height.
-            for story_height, story_bucket in \
-                    self.stories_by_bucket.iteritems():
-                # Skip this bucket if its story height is greater than
-                # the browser height.
-                if browser_height - story_height < 0:
-                    continue
-
-                # Fetch the rule representing the best feed that could
-                # accommodate a story of this height.
-                previous_rule = rules[browser_height - story_height]
-
-                # Find the highest-scoring story of this height not
-                # already used in the previous_rule, if any, and add it
-                # to a new candidate rule.
-                for story in story_bucket:
-                    if story not in previous_rule:
-                        possible_rules.append(previous_rule.copy())
-                        possible_rules[-1].add(story)
-                        break
-
-            # Select the highest-scoring rule from among our candidate rules.
-            rules[browser_height] = max(
-                possible_rules,
-                key=lambda rule: sum(
-                    self.stories_by_id[story_id].score for story_id in rule
-                )
-            )
-
-        # Build our optimal feed from the rule representing the browser
-        # height.
+        # Build our optimal feed from the set of stories.
         return "{} {} {}".format(
-            sum(
-                self.stories_by_id[story_id].score
-                for story_id in rules[browser_height]
-            ),
-            len(rules[browser_height]),
-            ' '.join(sorted(rules[browser_height]))
+            sum(self.stories_by_id[story_id].score for story_id in rule),
+            len(rule),
+            ' '.join(sorted(rule))
         )
 
 
